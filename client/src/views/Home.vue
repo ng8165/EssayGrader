024d82d3517e4c09c31f86e572e5c55718c308a8
Nelson Gou <nel4gou@gmail.com>
2022-12-25 16:02:43 -0800

    Merge Find and Submit into Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,65 @@
 <script lang="ts">
-import SubmitView from "./Submit.vue";
-import FindView from "./Find.vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
 export default {
-    components: {
-        SubmitView, FindView
+    setup() {
+        const submitName = ref("");
+        const submitEssay = ref("");
+        const findName = ref("");
+        const router = useRouter();
+
+        async function sendGrade() {
+            const res = await fetch("http://localhost:2020/", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ name: submitName.value, essay: submitEssay.value })
+            });
+
+            const data = await res.json();
+
+            if (res.ok) router.push(`/feedback/${data.id}`);
+            else alert(data.message);
+        }
+
+        async function findID() {
+            const res = await fetch(`http://localhost:2020/grade/name/${findName.value}`);
+            const data = await res.json();
+
+            if (res.ok) router.push(`/feedback/${data.id}`);
+            else alert(data.message);
+        }
+
+        return { submitName, submitEssay, findName, sendGrade, findID };
     }
 }
 </script>
 
 <template>
-    <SubmitView />
+    <h2 class="mb-6">Submit Essay</h2>
+
+    <form @submit.prevent="sendGrade">
+        <div class="mb-4">
+            <label for="name" class="mr-1">Name:</label>
+            <input name="name" id="name" required v-model="submitName" />
+        </div>
+
+        <label for="essay">Essay:</label>
+        <textarea name="essay" id="essay" class="block w-full mt-1 mb-4" rows="10" required v-model="submitEssay"></textarea>
+
+        <button type="submit" class="block bg-blue-500 text-white">Submit</button>
+    </form>
 
     <hr class="border-neutral-500 my-6" />
 
-    <FindView />
+    <h2 class="mb-6">Find Essay</h2>
+
+    <form @submit.prevent="findID">
+        <div class="mb-4">
+            <label for="name" class="mr-1">Name:</label>
+            <input name="name" id="name" required v-model="findName" />
+        </div>
+
+        <button type="submit" class="block bg-blue-500 text-white">Find</button>
+    </form>
 </template>
